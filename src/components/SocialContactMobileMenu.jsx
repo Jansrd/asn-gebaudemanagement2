6f5751b6a522670f5dae4e189ae3790b2dd3a76e
Jansrd <radivojacsrdjan@gmail.com>
2024-09-10 13:42:00 -0400

    iconSize fixed

--- src/components/SocialContactMobileMenu.jsx
+++ src/components/SocialContactMobileMenu.jsx
@@ -10,18 +10,19 @@ function SocialContactMobileMenu() {
         link="https://www.instagram.com/asn_gebaeudemanagement/"
         icon={<GrInstagram />}
         iconColor="rgb(193, 53, 132)"
+        iconSize="20px"
       />
       <SocialNetwork
         link="https://www.facebook.com/profile.php?id=61561726511044"
         icon={<FaFacebookSquare />}
         iconColor="rgb(24, 119, 242)"
-        iconSize="17px"
+        iconSize="21px"
       />
       <SocialNetwork
         link="https://wa.me/4917684560147"
         icon={<FaSquareWhatsapp />}
         iconColor="rgb(37, 211, 102)"
-        iconSize="17px"
+        iconSize="21px"
       />
     </div>
   );
